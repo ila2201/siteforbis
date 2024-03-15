6d1e74c3c4431f53e3f09d27d64f5b167da44103
ila2201 <103678079+ila2201@users.noreply.github.com>
2024-03-15 20:18:06 +0400

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Уроggg</title>
+    <link rel="stylesheet" href="1.css">
+</head>
+<body>
+    <?php
+        echo "<h1>hi</h1>";
+        $q = 1 + 4;
+        echo "<h2>$q</h2>";
+    ?>
+    <b class="e">First <br></b>
+    <b class="e u">First <br></b>
+    <b class="e r">First</b>
+</body>
+</html>
